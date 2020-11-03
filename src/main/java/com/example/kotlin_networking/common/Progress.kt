c4c58937979d1e6a367b1b82293816bbd4279006
xuantruong21 <xuantruongcarbon@gmail.com>
2020-11-03 07:44:33 +0700

    no message

--- src/main/java/com/example/kotlin_networking/common/Progress.kt
+++
@@ -1,5 +0,0 @@
-package com.example.kotlin_networking.common
-
-import java.io.Serializable
-
-class Progress(var currentBytes: Long, var totalBytes: Long) : Serializable
--- src/main/java/com/example/kotlin_networking/common/RequestType.kt
+++
@@ -1,7 +0,0 @@
-package com.example.kotlin_networking.common
-
-enum class RequestType {
-    SIMPLE,
-    DOWNLOAD,
-    MULTIPART
-}
